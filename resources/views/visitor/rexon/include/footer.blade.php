a705c96f791cf323edd228412b0797b170dfc256
International Climate Finance Cell <icfcerdbd@gmail.com>
2026-03-05 15:53:17 +0600

    Flixta theme
    new theme added
    
    Co-Authored-By: Firoz Ebna Jobaier <firoz.jobaier@gmail.com>

---
+++ resources/views/visitor/rexon/include/footer.blade.php
@@ -0,0 +1,42 @@
+<!-- footer area start -->
+<footer class="rs-footer-area rs-footer-one primary-bg">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-xl-6">
+                <div class="rs-footer-widget footer-1-col-1">
+                    <div class="rs-footer-widget-logo text-center">
+                        <a class="logo-black" href="index.html"><img src="assets/images/logo/logo-black.png"
+                                alt="logo"></a>
+                        <a class="logo-white" href="index.html"><img src="assets/images/logo/logo-white.png"
+                                alt="logo"></a>
+                    </div>
+                    <div class="rs-footer-widget-content">
+                        <div class="rs-footer-list-wrapper">
+                            <ul class="rs-footer-list">
+                                <li><a href="index.html">Home</a></li>
+                                <li><a href="about.html">About</a></li>
+                                <li><a href="services.html">Services</a></li>
+                                <li><a href="portfolio.html">Portfolio</a></li>
+                                <li><a href="blog.html">Blog</a></li>
+                                <li><a href="contact.html">Contact</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="rs-footer-copyright-area rs-copyright-one">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-xl-12">
+                    <div class="rs-footer-copyright text-center">
+                        <p>© <span id="year"></span> Flixta. Designed by <a href="https://rstheme.com">RSTheme</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- footer area end -->
---
+++ resources/views/visitor/rexon/layout/app-dark.blade.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html class="no-js" lang="zxx" dir="ltr" rs-theme="rs-theme-dark">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Shared on THEMELOCK.COM - CV/Resume One page || HTML Template</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Place favicon.ico in the root directory -->
+    <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.png">
+    <!-- CSS here -->
+    <link rel="stylesheet" href="assets/css/vendor/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/vendor/animate.min.css">
+    <link rel="stylesheet" href="assets/css/plugins/swiper.min.css">
+    <link rel="stylesheet" href="assets/css/plugins/nice-select.css">
+    <link rel="stylesheet" href="assets/css/vendor/magnific-popup.css">
+    <link rel="stylesheet" href="assets/css/vendor/fontawesome-pro.css">
+    <link rel="stylesheet" href="assets/css/vendor/spacing.css">
+    <link rel="stylesheet" href="assets/css/vendor/remixicon.css">
+    <link rel="stylesheet" href="assets/css/main.css">
+</head>
+
+<body class="rs-smoother-yes rtl">
+
+    <!-- preloader start -->
+    <div id="pre-load">
+        <div id="loader" class="loader">
+            <div class="loader-container">
+                <div class="loader-icon"><img src="assets/images/favicon.png" alt=""></div>
+            </div>
+        </div>
+    </div>
+    <!-- preloader end -->
+
+    @include('visitor.reeni.include.header')
+
+    @yield('content')
+
+    @include('visitor.reeni.include.footer')
+
+    <!-- Backtotop start -->
+    <div class="backtotop-wrap cursor-pointer">
+        <svg class="backtotop-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+            <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
+        </svg>
+    </div>
+    <!-- Backtotop end -->
+
+    <!-- JS here -->
+    <script src="assets/js/vendor/jquery-3.7.1.min.js"></script>
+    <script src="assets/js/plugins/waypoints.min.js"></script>
+    <script src="assets/js/vendor/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/plugins/meanmenu.min.js"></script>
+    <script src="assets/js/plugins/swiper.min.js"></script>
+    <script src="assets/js/plugins/wow.js"></script>
+    <script src="assets/js/vendor/magnific-popup.min.js"></script>
+    <script src="assets/js/vendor/isotope.pkgd.min.js"></script>
+    <script src="assets/js/vendor/imagesloaded.pkgd.min.js"></script>
+    <script src="assets/js/plugins/nice-select.min.js"></script>
+    <script src="assets/js/plugins/jarallax.min.js"></script>
+    <script src="assets/js/vendor/ajax-form.js"></script>
+    <script src="assets/js/plugins/easypie.js"></script>
+    <script src="assets/js/plugins/headding-title.js"></script>
+    <script src="assets/js/plugins/lenis.min.js"></script>
+    <script src="assets/js/plugins/gsap.min.js"></script>
+    <script src="assets/js/plugins/rs-anim-int.js"></script>
+    <script src="assets/js/plugins/rs-scroll-trigger.min.js"></script>
+    <script src="assets/js/plugins/rs-splitText.min.js"></script>
+    <script src="assets/js/plugins/jquery.lettering.js"></script>
+    <script src="assets/js/plugins/parallax-effect.min.js"></script>
+    <script src="assets/js/vendor/purecounter.js"></script>
+    <script src="assets/js/vendor/dark-light.js"></script>
+    <script src="assets/js/main.js"></script>
+</body>
+
+</html>
