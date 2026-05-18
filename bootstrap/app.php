c1950f85d3640a1d45cd6bcf391f2b8da18c9da6
Firoz Ebna Jobaier <firoz.jobaier@gmail.com>
2026-05-18 23:49:58 +0600

    Website analytics
    Add yenHunter/laravel-website-analytics package

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,16 +3,21 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Yenhunter\LaravelWebsiteAnalytics\Http\Middleware\TrackVisitor;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'visitor' => \App\Http\Middleware\SetVisitorTheme::class,
+        ]);
+
+        $middleware->web(append: [
+            TrackVisitor::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
